ece191e29e3643837ef543f1422dbdc80974d56e
Marco Siweris <s8masiwe@stud.uni-saarland.de>
2017-06-01 17:24:55 +0200

    debug

--- src/unity/Assets/Scripts/PlayerController.cs
+++ src/unity/Assets/Scripts/PlayerController.cs
@@ -2,26 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//[RequireComponent(typeof(SphereCollider))]
-
 public class PlayerController : MonoBehaviour {
 
     public float speed;
     private Rigidbody rb;
     private Vector3 offset;
     private Vector3 screenPoint;
-    // private Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    //[SerializeField] float treshhold = 0.5f;
     float depth = 44.5f;
-
-   
-
-    
-
 
     void Start () {
         rb = GetComponent<Rigidbody>();
-        //Screen.showCursor = false;
     }
 
     void Update()
@@ -31,8 +21,4 @@ public class PlayerController : MonoBehaviour {
         rb.AddForce(wantedPos * speed);
     }
 
-  
-    
-   
-   */
 }
