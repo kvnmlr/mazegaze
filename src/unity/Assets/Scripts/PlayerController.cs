778b2a5314f541036ad7501710bc5f3177c3753c
Marco Siweris <s8masiwe@stud.uni-saarland.de>
2017-06-01 17:22:09 +0200

    object folgt mauszeiger

--- src/unity/Assets/Scripts/PlayerController.cs
+++ src/unity/Assets/Scripts/PlayerController.cs
@@ -10,75 +10,29 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
     private Vector3 offset;
     private Vector3 screenPoint;
-    private Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    [SerializeField] float treshhold = 0.5f;
+    // private Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //[SerializeField] float treshhold = 0.5f;
+    float depth = 44.5f;
+
+   
+
+    
 
 
     void Start () {
         rb = GetComponent<Rigidbody>();
+        //Screen.showCursor = false;
     }
-    
+
     void Update()
     {
-        Vector3 mouseIntoWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        if(mouseIntoWorldPos.x > gameObject.transform.position.x + treshhold)
-        {
-            transform.Translate(1, 0, 0);
-        }else if(mouseIntoWorldPos.x > gameObject.transform.position.x - treshhold)
-        {
-            transform.Translate(-1, 0, 0);
-        }else if(mouseIntoWorldPos.z > gameObject.transform.position.z + treshhold)
-        {
-            transform.Translate(0, 0, 1);
-        }else if(mouseIntoWorldPos.z > gameObject.transform.position.z - treshhold)
-        {
-            transform.Translate(0, 0, -1);
-        }
-        
-        
-
-    }
-    
-    
-    /*
-    void FixedUpdate () {
-
-
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce(movement * speed);
-    }
-    
-    private void FixedUpdate()
-    {
-
-        float horizontal = Input.mousePosition.x;
-        float vertical = Input.mousePosition.z;
-
-        Vector3 movement = new Vector3(horizontal, 0.0f,vertical);
-
-        rb.AddForce(movement * speed);
-    }
-    
-    void OnMouseDown()
-    {
-        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-        
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 wantedPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, depth));
+        rb.AddForce(wantedPos * speed);
     }
 
-    void OnMouseDrag()
-    {
-        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-        transform.position = curPosition;
-
-
-    }*/
-
+  
+    
+   
+   */
 }
