8a14df66fbd8c95eb0dd9ce96d1527ac6741e7f9
Lena Hornberger <lena96@tuxlan.de>
2017-06-18 15:59:02 +0200

    Created simple Menu with Play and Quit

---
+++ src/unity/Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
