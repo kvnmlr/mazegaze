627f149afbae7e9ccd63c9a38518d8035e8740f1
Mueller <Kevin.Mueller@scheer-group.com>
2017-08-14 14:41:10 +0200

    pupil settings screen elements

---
+++ src/unity/Assets/Scripts/UI/ClientSettingsUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClientSettingsUI : MonoBehaviour {
+    public InputField clientName;
+    public InputField ipAddress;
+    public InputField port;
+    public InputField surfaceName;
+    public Button connect;
+    public Button calibrate;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
